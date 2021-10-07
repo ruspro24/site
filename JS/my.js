e3b68321e85a77f0112bcd403018102d73554ad7
Utvenko Ruslan <ruslan.utvenko@gmail.com>
2021-10-07 15:52:08 +0300

    m

--- JS/my.js
+++ JS/my.js
@@ -1,5 +1,6 @@
 var wow = new WOW(
     {
+      mobile: false,
       boxClass:     'wow',      // animated element css class (default is wow)
       animateClass: 'animate__animated', // animation css class (default is animated)
       offset:       0,          // distance to the element when triggering the animation (default is 0)
